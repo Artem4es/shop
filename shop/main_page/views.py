9c3fb2a2649e6f1b6f0adf5bc29c28da1ca13554
Артём Ежов <dragster1001@yandex.ru>
2023-02-07 14:07:29 +0300

    add bot, improve design

--- shop/main_page/views.py
+++ shop/main_page/views.py
@@ -1,16 +1,25 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
 
 
 from main_page.forms import OrderForm
+from .telegram import bot, admin_id
 
 
 def index(request):
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
-
             form.save()
-            # post.save()
+            name = form.cleaned_data.get('name')
+            bike = form.cleaned_data.get('bike')
+            phone = form.cleaned_data.get('phone')
+            comment =form.cleaned_data.get('comment')
+
+            bot.send_message(chat_id=admin_id, text=f'Ура, новая заявка от {name}!')
+            bot.send_message(chat_id=admin_id, text=f'Хочет прошить мотоцикл: {bike}')
+            bot.send_message(chat_id=admin_id, text=phone)
+            if comment:
+                bot.send_message(chat_id=admin_id, text=comment)
             return render(request, 'suc.html')
         return render(request, 'error.html', {'form': form})
 
