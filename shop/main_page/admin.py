54eb9e4d78338481f802840494bc5b503684ee69
Артём Ежов <dragster1001@yandex.ru>
2023-02-07 17:19:03 +0300

    add admin_zone

--- shop/main_page/admin.py
+++ shop/main_page/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bike','phone', 'comment', 'order_date')
+    search_fields = ('name', 'bike', 'comment')
+    list_filter = ('order_date',)
+    empty_value_display = '-пусто-'
+
+admin.site.register(Order, OrderAdmin)
